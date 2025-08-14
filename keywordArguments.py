e33a6e095dfdad60a1288aaaeeb8459404fe30fa
Margarit Kyurkchyan <margarit.kyurkchyan@esterox.am>
2025-08-14 13:55:42 +0400

    Special parameters, positional only, Keyword only

--- keywordArguments.py
+++ keywordArguments.py
@@ -23,6 +23,7 @@ def display_info(first, second, *extras, category, detail, **options):
     print("Extra positional args:", extras)
     print("Keyword options:", options)
 
+
 # Example usage
 display_info(
     10,
@@ -33,3 +34,33 @@ display_info(
     author="Orwell",
     year=1949
 )
+
+
+# Special parameters
+def standard_arg(arg):
+    print(arg)
+
+
+def pos_only_arg(arg, /):
+    print(arg)
+
+
+def kwd_only_arg(*, arg):
+    print(arg)
+
+
+def combined_example(pos_only, /, standard, *, kwd_only):
+    print(pos_only, standard, kwd_only)
+
+
+standard_arg(2)
+standard_arg(arg=2)
+
+pos_only_arg(1)
+# pos_only_arg(arg=1)
+
+combined_example(1, 2, kwd_only=3)
+combined_example(1, standard=2, kwd_only=3)
+# combined_example(1, 2, 3)
+# combined_example(pos_only=1, standard=2, kwd_only=3)
+
