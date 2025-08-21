00a057acebe28d159c291df34d172e9be45cb8c3
Margarit Kyurkchyan <margarit.kyurkchyan@esterox.am>
2025-08-21 18:53:58 +0400

    Fancier Output Formatting

---
+++ imports.py
@@ -0,0 +1,15 @@
+s = 'Hello, world.'
+print(str(s))
+print(repr(s))
+print(str(1/7))
+x = 10 * 3.25
+y = 200 * 200
+s = 'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
+print(s)
+# The repr() of a string adds string quotes and backslashes:
+hello = 'hello, world\n'
+hellos = repr(hello)
+print(hellos)
+'hello, world\n'
+# The argument to repr() may be any Python object:
+repr((x, y, ('spam', 'eggs')))
