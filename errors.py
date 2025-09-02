7db1d32f14a4070b1424ddc69d5d1299474e7be8
Margarit Kyurkchyan <margarit.kyurkchyan@esterox.am>
2025-09-02 11:24:41 +0400

    Exeption example

--- errors.py
+++ errors.py
@@ -1,15 +1,18 @@
-class MyContextManager:
-    def __enter__(self):
-        print("Entering the context")
-        return self
+class B(Exception):
+    pass
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        print("Exiting the context")
-        if exc_type:
-            print(f"Exception Type: {exc_type}")
-            print(f"Exception Value: {exc_value}")
-        return True  # suppress the exception
+class C(B):
+    pass
 
-with MyContextManager() as cm:
-    print("Inside the context")
-    raise ValueError("An error occurred")
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except B:
+        print("B")
+    except D:
+        print("D")
+    except C:
+        print("C")
